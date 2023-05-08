6278373c40bd13f503d88fd3602977b7e1997ba0
ALexd-78 <alexd-78@yandex.ru>
2023-05-09 02:06:38 +0300

    configure DATABASE in settings.py

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'User {name} with email {email} send message: {message}')
+
+    return render(request, 'main/index.html')
+
